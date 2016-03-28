ed1c6694c26621f2ed7b5fe3551932536e88bdca
Michael Chrisco <michaelc@cvtrust.org>
2016-03-28 11:15:08 -0700

    init

---
+++ spec/ReadOnlyTraitSpec.php
@@ -0,0 +1,77 @@
+<?php
+require_once('src/ReadOnlyTrait.php');
+// use Michael\ReadOnlyTrait;
+use Illuminate\Database\Eloquent\Model;
+class User extends Illuminate\Database\Eloquent\Model {
+  use Michael\ReadOnly\ReadOnlyTrait;
+}
+
+describe("User", function() {
+  describe("::save()", function(){
+
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->save([]))->toBe(false);
+        unset($user);
+    });
+ });
+  describe("::update()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->update([]))->toBe(false);
+        unset($user);
+      });
+   });
+  describe("::firstOrCreate()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->firstOrCreate([]))->toBe(false);
+        unset($user);
+    });
+  });
+  describe("::firstOrNew()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->firstOrNew([]))->toBe(false);
+        unset($user);
+    });
+   });
+  describe("::delete()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->delete())->toBe(false);
+        unset($user);
+    });
+   });
+  describe("::destroy()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->destroy(1))->toBe(false);
+        unset($user);
+    });
+   });
+  describe("::restore()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->restore())->toBe(false);
+        unset($user);
+    });
+   });
+  describe("::forceDelete()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->forceDelete())->toBe(false);
+        unset($user);
+    });
+   });
+  describe("::test()", function(){
+    it("is expected to return false", function() {
+        $user = new User;
+        expect($user->test())->toBe(false);
+        unset($user);
+    });
+   });
+});
+
+
+ ?>
---
+++ src/ReadOnlyTrait.php
@@ -0,0 +1,39 @@
+<?php
+namespace Michael\ReadOnly;
+trait ReadOnlyTrait {
+  public function save(array $options = []){
+    return false;
+  }
+
+  public function update(array $attributes = [], array $options = []){
+    return false;
+  }
+
+  static function firstOrCreate(array $arr){
+    return false;
+  }
+
+  static function firstOrNew(array $arr){
+    return false;
+  }
+
+  public function delete(){
+    return false;
+  }
+
+  static function destroy($ids){
+    return false;
+  }
+
+  public function restore(){
+    return false;
+  }
+
+  public function forceDelete(){
+    return false;
+  }
+
+  public function test(){
+    return false;
+  }
+}
