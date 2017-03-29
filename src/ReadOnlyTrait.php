48e11b1044199bdba973b3acfe8d8f7f03600f86
aptdavid <dcolon@allpointstech.net>
2017-03-29 11:40:15 -0400

    Update ReadOnlyTrait.php

--- src/ReadOnlyTrait.php
+++ src/ReadOnlyTrait.php
@@ -161,4 +161,24 @@ trait ReadOnlyTrait {
       $class = get_called_class();
       throw new ReadOnlyException("Not allowed to persist changes in read-only model {$class}");
     }
+  
+   /**
+   * throws ReadOnlyException on insert
+   * @method insert
+   *
+   */
+    public function insert(){
+      $class = get_called_class();
+      throw new ReadOnlyException("Not allowed to persist changes in read-only model {$class}");
+    }
+  
+   /**
+   * throws ReadOnlyException on emptyTable
+   * @method emptyTable
+   *
+   */
+    public function emptyTable(){
+      $class = get_called_class();
+      throw new ReadOnlyException("Not allowed to persist changes in read-only model {$class}");
+    }
 }
