3ae716fa9d26a20bb53c27d7c3ef83c72a0229f4
Michael Chrisco <michaelachrisco@users.noreply.github.com>
2025-11-20 16:27:31 -0800

    Allow nullable previous Throwable in constructor
    Fixes CI issue in PHP 8.3+

--- src/ReadOnlyException.php
+++ src/ReadOnlyException.php
@@ -8,7 +8,7 @@ class ReadOnlyException extends \RuntimeException
      * @param string $modelClassName
      * {@inheritDoc}
      */
-    public function __construct(string $functionName, string $modelClassName, int $code = 0, \Throwable $previous = null)
+    public function __construct(string $functionName, string $modelClassName, int $code = 0, ?\Throwable $previous = null)
     {
         $message = sprintf('Calling [%s] method on read-only model [%s] is not allowed.', $functionName, $modelClassName);
         parent::__construct($message, $code, $previous);
